b734bdf2342db06976e02c67f0097e6a966d82ed
Joachim Karlsson <Joakar@wcom.se>
2014-06-03 22:04:39 +0200

    1. Different inputs for different sql types

--- Helpers/Html/AngularHtml.cs
+++ Helpers/Html/AngularHtml.cs
@@ -11,7 +11,52 @@ namespace Helpers
     {
         public string RenderAngularInput(Column column, string tableName)
         {
-            return string.Format(@"<input type=""text"" ng-model=""{0}.{1}""/>", tableName, column.Name);
+            var angularModel = string.Concat("{{", tableName, ".", column.Name, "}}");
+
+            if (column.InPrimaryKey)
+            {
+                return string.Format(@"<span>{2}</span>", tableName, column.Name, angularModel);
+            }
+
+            switch (column.DataType.SqlDataType)
+            {
+                case SqlDataType.Time:
+                case SqlDataType.Timestamp:
+                case SqlDataType.SmallDateTime:
+                case SqlDataType.Date:
+                case SqlDataType.DateTime:
+                case SqlDataType.DateTime2:
+                case SqlDataType.DateTimeOffset:
+                    var angularDateModel = string.Concat("{{", tableName, ".", column.Name, "  | date:'yyyy-MM-dd HH:mm'}}");
+                    return string.Format(@"<a onaftersave=""saveColumn({0})"" editable-date=""{0}.{1}"">{2}</a>", tableName, column.Name, angularDateModel);
+                case SqlDataType.SmallMoney:
+                case SqlDataType.TinyInt:
+                case SqlDataType.BigInt:
+                case SqlDataType.SmallInt:
+                case SqlDataType.Bit:
+                case SqlDataType.Int:
+                case SqlDataType.Money:
+                case SqlDataType.Decimal:
+                case SqlDataType.Float:
+                case SqlDataType.Numeric:
+                    return string.Format(@"<a onaftersave=""saveColumn({0})"" editable-number=""{0}.{1}"">{2}</a>", tableName, column.Name, angularModel);
+                case SqlDataType.VarBinary:
+                case SqlDataType.VarBinaryMax:
+                case SqlDataType.VarChar:
+                case SqlDataType.VarCharMax:
+                case SqlDataType.Variant:
+                case SqlDataType.Xml:
+                case SqlDataType.Text:
+                case SqlDataType.UniqueIdentifier:
+                case SqlDataType.Char:
+                case SqlDataType.NChar:
+                case SqlDataType.NText:
+                case SqlDataType.NVarChar:
+                case SqlDataType.NVarCharMax:
+                    return string.Format(@"<a onaftersave=""saveColumn({0})"" editable-text=""{0}.{1}"">{2}</a>", tableName, column.Name, angularModel);
+                default:
+                    return string.Format(@"<a onaftersave=""saveColumn({0})"" editable-text=""{0}.{1}"">{2}</a>", tableName, column.Name, angularModel);
+            }
         }
 
         public string RenderAngularSaveButton()
