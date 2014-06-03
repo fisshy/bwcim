038b0c4747e0036221477ba41bc5d0c4aabc1f61
Joachim Karlsson <Joakar@wcom.se>
2014-06-03 22:05:17 +0200

    1. Added x-editable and angular loader. 2. Moved icons.

--- Bwcim/App_Start/BundleConfig.cs
+++ Bwcim/App_Start/BundleConfig.cs
@@ -9,13 +9,18 @@ namespace Bwcim
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
-                        "~/Scripts/*.js"));
+                            "~/Scripts/libs/*.js",
+                            "~/Scripts/*.js"
+                        ));
 
             
 
-            bundles.Add(new StyleBundle("~/Content/css/bundled").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                            "~/Content/xeditable.css",
+                            "~/Content/loading-bar.css",
+                            "~/Content/bootstrap.css",
+                            "~/Content/site.css"
+                        ));
         }
     }
 }
